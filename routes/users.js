924dc2a22321ab383f6745beaae29bf654703df3
Thanakorn Khuntamanee <guytanakornkhuntamanee@gmail.com>
2025-04-24 13:28:56 +0700

    Add authorize dentist when use getUser

--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,6 @@ const router = express.Router();
 const {protect, authorize} = require('../middleware/auth');
 
 router.route('/').get(protect, authorize('admin'), getUsers);
-router.route('/:id').get(protect, authorize('admin'), getUser).put(protect, authorize('admin'), updateUserRole);
+router.route('/:id').get(protect, authorize('admin', 'dentist'), getUser).put(protect, authorize('admin'), updateUserRole);
 
 module.exports = router;
