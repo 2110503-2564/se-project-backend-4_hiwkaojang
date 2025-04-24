d4abdeda67f7b94f47d3e25d79220943601e89ff
Karin1844 <Karinsitthibut@gmail.com>
2025-04-24 21:25:49 +0700

    add dentist to create booking

--- routes/bookings.js
+++ routes/bookings.js
@@ -5,7 +5,7 @@ const router = express.Router({mergeParams:true});
 
 const {protect, authorize} = require('../middleware/auth')
 
-router.route('/').get(protect, authorize('admin', 'user', 'dentist'), getBookings).post(protect, authorize('admin', 'user'), addBooking);
+router.route('/').get(protect, authorize('admin', 'user', 'dentist'), getBookings).post(protect, authorize('admin', 'user','dentist'), addBooking);
 router.route('/:id').get(protect, authorize('admin', 'user', 'dentist'), getBooking).put(protect, authorize('admin', 'user', 'dentist'), updateBooking).delete(protect, authorize('admin'), deleteBooking);
 
 router.route('/patientHistory/:userId').get(protect, authorize('dentist'), getPatientHistory);
