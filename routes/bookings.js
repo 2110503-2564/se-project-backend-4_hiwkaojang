f9d6c9f9a9c62565c62f415effa2d23490bb277a
Thanakorn Khuntamanee <guytanakornkhuntamanee@gmail.com>
2025-04-26 11:00:46 +0700

    Add swagger schemas for booking

--- routes/bookings.js
+++ routes/bookings.js
@@ -35,7 +35,47 @@ router.route('/patientHistory/:userId')
  *   schemas:
  *     Booking:
  *       type: object
- *       
+ *       required:
+ *         - bookingDate
+ *         - user
+ *         - dentist
+ *       properties:
+ *         id:
+ *           type: string
+ *           format: uuid
+ *           description: The auto-generated unique identifier for the booking
+ *           example: 67fde5505a0148bd60617094
+ *         bookingDate:
+ *           type: string
+ *           format: date-time
+ *           description: The date and time of the booking
+ *         user:
+ *           type: string
+ *           format: uuid
+ *           description: The ID of the user making the booking
+ *         dentist:
+ *           type: string
+ *           format: uuid
+ *           description: The ID of the dentist for the booking
+ *         status:
+ *           type: string
+ *           enum: [upcoming, completed, cancelled, confirmed, blocked]
+ *           description: The status of the booking
+ *         createdAt:
+ *           type: string
+ *           format: date-time
+ *           description: The date and time when the booking was created
+ *         treatmentDetail:
+ *           type: string
+ *           description: Details about the treatment
+ *       example:
+ *         id: 67fde7ded883ed6a5f67590d
+ *         bookingDate: 2025-09-01T17:00:00.000+00:00
+ *         user: 67fde38d5a0148bd60617086
+ *         dentist: 67fde38d5a0148bd60617087
+ *         status: completed
+ *         createdAt: 2025-04-15T05:00:14.407+00:00
+ *         treatmentDetail: "Performed teeth cleaning and fluoride treatment to prevent cavities and maintain oral hygiene."
  */
 
 module.exports = router;
